3b5586093b2a673de8ff792d1ceebf46acf817ee
Tara Z. Manicsic <tzmanics@gmail.com>
2024-01-29 00:34:04 -0500

    changes styling and extra text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,7 +2,7 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">LaiFE COACH</h1>
     <p>
-      You know, I always say that I'm just a bundle of positivity, motivation, and love wrapped up in a big virtual hug! I'm here to help you be the best version of yourself and to spread a little joy along the way. After all, as Maya Angelou said, "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style." So, how can I help uplift your day, my lovely friend in Ferristown?
+        You know, I always say that I'm just a bundle of positivity, motivation, and love wrapped up in a big virtual hug! I'm here to help you be the best version of yourself and to spread a little joy along the way. After all, as Maya Angelou said, "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style." So, how can I help uplift your day?
     </p>
     <p>
       <a className="underline" href="https://github.com/ascorbic/daneel#readme">
